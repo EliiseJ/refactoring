96dee7f7c12cafbf1288902bd67a407a5e706ff7
kasparkivistik <kkivis@ttu.ee>
2018-03-14 15:13:12 +0200

    Added test skeletons

--- test/rental/RentalPriceCalculatorTest.java
+++ test/rental/RentalPriceCalculatorTest.java
@@ -6,14 +6,32 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class RentalPriceCalculatorTest {
-	
-	@Before
-	public void beforeEachTest() {
-		// this method is called before each test
-	}
+
+	private RentalPriceCalculator rentalPriceCalculator;
+
+    @Before
+    public void setUp() throws Exception {
+        rentalPriceCalculator = new RentalPriceCalculator();
+    }
 
 	@Test
 	public void test() {
 		fail("Not yet implemented");
 	}
+
+	@Test
+    public void testCanDriverRentCar() {}
+
+    @Test
+    public void testGetRentalPrice() {}
+
+    @Test
+    public void testGetExtraFees() {
+    }
+
+    @Test
+    public void testGetPriceMultiplier() {}
+
+    @Test
+    public void testIsDriverOldEnough() {}
 }
