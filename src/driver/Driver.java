da6eb054b686a49aa2b51dba5d0f760ff6d48d84
kasparkivistik <kkivis@ttu.ee>
2018-03-14 14:11:08 +0200

    Added driver class

---
+++ src/driver/Driver.java
@@ -0,0 +1,20 @@
+package driver;
+
+public class Driver {
+
+    private int age;
+    private boolean licence;
+    private boolean hasCausedAccidents;
+
+    public boolean isHasCausedAccidents() {
+        return hasCausedAccidents;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isLicence() {
+        return licence;
+    }
+}
