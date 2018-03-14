4e9054030ab72f762ff52efc5e2fbf35f4b5e9ab
kasparkivistik <kkivis@ttu.ee>
2018-03-14 15:10:02 +0200

    Added constant values

--- src/rental/RentalPriceCalculator.java
+++ src/rental/RentalPriceCalculator.java
@@ -7,6 +7,14 @@ import java.math.BigDecimal;
 
 public class RentalPriceCalculator {
 
+    public static final int YOUNG_DRIVER_HIGH_CAR_CLASS = 4;
+    public static final int YOUNG_DRIVER_HIGH_CAR_CLASS_MIN_AGE = 25;
+    public static final int YOUNG_DRIVER_HIGH_CAR_CLASS_MULTIPLIER = 2;
+    public static final int NEW_LICENSE = 3;
+    public static final double NEW_LICENSE_MULTIPLIER = 1.3;
+    public static final int MINIMAL_AGE_FOR_DRIVING = 18;
+    public static final int MINIMAL_AGE_FOR_HIGH_CLASS_CAR = 21;
+
     // age - age of driver
     // licence - number of full years person holds driving licence
     // clazz - class of the car from 1 (smallest) to 5 (largest) that person wishes to rent
@@ -28,20 +36,27 @@ public class RentalPriceCalculator {
 			rentalprice = rentalprice * 2;
 		}
 		*/
-        if (driver.getTimeOfLicense() < 1) {
-            throw new IllegalArgumentException("Driver must hold driving licence at least for one year. Can not rent a car!");
-        }
 	/*
 		if (rentalData.getLicence() < 3) {
 			rentalprice = rentalprice * 1.3;
 		}
 		*/
-        rentalprice = getExtraFees(driver);
 
         if (rentalprice > 1000) {
             return 1000.00;
         }
         return rentalprice;
+    }
+
+    public boolean canDriverRentCar(Driver driver) {
+        if (driver.getTimeOfLicense() < 1) {
+            throw new IllegalArgumentException("Driver must hold driving licence at least for one year. Can not rent a car!");
+        }
+        return true;
+    }
+
+    public BigDecimal getRentalPrice(Driver driver, int carClass, boolean isSeason) {
+        return BigDecimal.valueOf(driver.getAge());
     }
 
     private BigDecimal getExtraFees(Driver driver) {
@@ -53,20 +68,20 @@ public class RentalPriceCalculator {
 
     public BigDecimal getPriceMultiplier(Driver driver, int carClass, boolean isSeason) {
         BigDecimal multiplier = BigDecimal.ONE;
-        if (carClass >= 4 && driver.getAge() <= 25 && isSeason) {
-            return multiplier.multiply(BigDecimal.valueOf(2));
+        if (carClass >= YOUNG_DRIVER_HIGH_CAR_CLASS && driver.getAge() <= YOUNG_DRIVER_HIGH_CAR_CLASS_MIN_AGE && isSeason) {
+            return multiplier.multiply(BigDecimal.valueOf(YOUNG_DRIVER_HIGH_CAR_CLASS_MULTIPLIER));
         }
-        if (driver.getTimeOfLicense() < 3) {
-            return multiplier.multiply(BigDecimal.valueOf(1.3));
+        if (driver.getTimeOfLicense() < NEW_LICENSE) {
+            return multiplier.multiply(BigDecimal.valueOf(NEW_LICENSE_MULTIPLIER));
         }
         return multiplier;
     }
 
     public boolean isDriverOldEnough(Driver driver, int carClass) {
-        if (driver.getAge() < 18) {
+        if (driver.getAge() < MINIMAL_AGE_FOR_DRIVING) {
             throw new IllegalArgumentException("Driver too young - cannot quote the price");
         }
-        if (driver.getAge() <= 21 && carClass > 2) {
+        if (driver.getAge() <= MINIMAL_AGE_FOR_HIGH_CLASS_CAR && carClass > 2) {
             throw new IllegalArgumentException("Driver must hold driving licence at least for one year. Can not rent a car!");
         }
         return true;
